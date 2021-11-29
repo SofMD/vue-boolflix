89589aeb36d09d9582fe7ff4300fa44b3ca04b07
sofMD <xsofiamaria.32@gmail.com>
2021-11-29 12:21:58 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
 
+    <Header/>
+
   </div>
 </template>
 
 <script>
-
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'App',
   components: {
+    Header,
 
   }
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+
+<header>
+    <form action="">
+        <input type="text" placeholder="Cerca film e serie TV">
+        <button>Search</button>
+    </form>
+</header>
+  
+</template>
+
+<script>
+export default {
+    name: 'Header', 
+
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    padding: 20px;
+
+    button {
+        margin: 10px;
+    }
+}
+
+
+</style>
